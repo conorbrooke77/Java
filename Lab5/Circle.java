33841a7da8b0b9e215e705aba8736690e327c546
conorbrooke77 <68202359+conorbrooke77@users.noreply.github.com>
2021-11-02 21:00:13 +0000

    syncing files

---
+++ Lab5/Circle.java
@@ -0,0 +1,17 @@
+public class Circle extends Point{
+    private int radius;
+
+    public Circle(int x, int y, int raduis) {
+        super(x, y);
+        
+        this.radius = raduis;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+} 
---
+++ Lab5/Point.java
@@ -0,0 +1,32 @@
+public class Point {
+    protected int x;
+    protected int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setCoordinateX(int x) {
+        this.x = x;
+    }
+
+    public void setCoordinateY(int y) {
+        this.y = y;
+    }
+
+    public int getCoordinateX() {
+        return x;
+    }
+
+    public int getCoordinateY() {
+        return y;
+    }
+
+    public String toString() {
+        String temp = "";
+
+        temp += "\nThe X coordinate is " + x + "\nThe Y coordinate is " + y;
+        return temp;
+    }
+}  
